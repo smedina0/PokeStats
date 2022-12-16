332d5bd2c5dd888288ead51e89e4de673398c064
Steven Medina <smedina2494@gmail.com>
2022-12-16 01:32:40 -0500

    Feat: Completes base project

--- js/script.js
+++ js/script.js
@@ -1,21 +1,57 @@
-// Javascript Variables
-
-
-
-
-
 // Pokemon Scripts
-let pokemonName = "";
+let pokemonData;
 
-const promise = $.ajax({
+const empty = "";
+
+// Pokemon Stats
+const $hp = $('#hp');
+const $atk = $('#atk');
+const $def = $('#def');
+const $specialAtk = $('#specialAtk');
+const $specialDef = $('#specialDef');
+const $speed = $('#speed');
+const $nameReplace = $('#pkmnName');
+const $pokemonImg = $('#pokemonImg');
+// const $pokemonType = $('#pokemonType')
+
+
+// Taking the input
+const $input = $('input[type="text"]');
+
+$("form").on("submit", handleGetData);
+
+function handleGetData(event) {
+  event.preventDefault()
+  pokemonName = $input.val().toLowerCase();
+  $input.val('');
+$.ajax({
     url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
-  })
-  
-  promise.then(
+  }).then(
     (data) => {
+      pokemonData = data;
       console.log(data)
+      statSet();
+      weakStrengthSet();
+
+      
     },
     (error) => {
       console.log("bad request: ", error)
     }
   )
+}
+
+  function statSet() {
+    $hp.text(pokemonData.stats['0'].base_stat);
+    $atk.text(pokemonData.stats['1'].base_stat);
+    $def.text(pokemonData.stats['2'].base_stat);
+    $specialAtk.text(pokemonData.stats['3'].base_stat);
+    $specialDef.text(pokemonData.stats['4'].base_stat);
+    $speed.text(pokemonData.stats['5'].base_stat);
+    $nameReplace.text(pokemonData.name)
+    $pokemonImg.attr("src",pokemonData.sprites.front_default);
+  }
+
+  function weakStrengthSet() {
+    $pokemonType = pokemonData.types[0].type.name;
+  }
