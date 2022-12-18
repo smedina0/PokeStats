93da2c1e5da30326bb681665380b1e4fa1ba2028
Steven Medina <smedina2494@gmail.com>
2022-12-17 19:12:58 -0500

    Feat: Completes everything except dual type effectiveness logic

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ const $speed = $('#speed');
 const $nameReplace = $('#pkmnName');
 const $pokemonImg = $('#pokemonImg');
 const $typed = $('#typed');
+const $type1 = $('#type1');
 
 // Damage Changes
 
@@ -38,7 +39,6 @@ $.ajax({
   }).then(
     (data) => {
       pokemonData = data;
-      // console.log(data)
       statSet();
     if(pokemonData.types.length == 1){
       determineType1();
@@ -75,6 +75,7 @@ $.ajax({
     $pokemonType1 = pokemonData.types[0].type.name;
     console.log("pokemon first type is " + $pokemonType1);
     $typed.text($pokemonType1);
+    $type1.text($pokemonType1);
 
     // When the first type is determined, it's weaknesses will be determined
     determineWeakness1()
@@ -105,7 +106,7 @@ function determineWeakness1() {
   }).then(
     (data) => {
       damageType1 = data;
-      console.log(data)
+      // console.log(data)
 
       assignDamage1()
   
@@ -130,19 +131,47 @@ function determineWeakness2() {
   )
 }
 
+// FIRST TYPE
+
+
+// This determines the first type's values and assigns them to a location in the dom appropriate with their relative effectiveness
+
 function assignDamage1(){
+
+  // Initializing Damage Arrays for first type
+
+  const doubleDamageArr1 = [];
+  const halfDamageArr1 = [];
+  
+
+  // This ensures all types that are not mentioned are indicated as "normal" damage
 
   $("#regularDamage").append(`<li> All other types</li>`)
   
+  // These for loops go through all damage relation arrays and lists the specific types associated with each damage change for the first type
 
   for (let i = 0; i < damageType1.damage_relations.double_damage_from.length; i++) {
+    // console.log(damageType1.damage_relations.double_damage_from[i].name)  
  
-    $("#doubleDamageList").append(`<li>${damageType1.damage_relations.double_damage_from[i].name}</li>`)
+    $("#doubleDamageList").append(`<li>${damageType1.damage_relations.double_damage_from[i].name}</li>`);
+
+    // This creates an array containing all double damage weaknesses to the first type
+  
+   doubleDamageArr1.push(damageType1.damage_relations.double_damage_from[i].name);
   }
+
+   // logs the new array for testing
+   console.log("double damage array for first type is " + doubleDamageArr1);
 
   for (let i = 0; i < damageType1.damage_relations.half_damage_from.length; i++) {
     $("#halfDamageList").append(`<li>${damageType1.damage_relations.half_damage_from[i].name}</li>`)
+
+     // This creates an array containing all half damage weaknesses to the first type
+  
+   halfDamageArr1.push(damageType1.damage_relations.half_damage_from[i].name);
   }
+
+  console.log("half damage array for first type is " + halfDamageArr1);
 
   for (let i = 0; i < damageType1.damage_relations.no_damage_from.length; i++) {
     // console.log(damageType1.damage_relations.half_damage_from[i].name)
@@ -150,17 +179,49 @@ function assignDamage1(){
 
   }
 }
+
+
+// SECOND TYPE
+
+
+// This determines the second type's values and assigns them to a location in the dom appropriate with their relative effectiveness
   function assignDamage2(){
+
+    // Initializing Damage Arrays for second type
+  const doubleDamageArr2 = [];
+  const halfDamageArr2 = [];
+
+
 
   for (let i = 0; i < damageType2.damage_relations.double_damage_from.length; i++) {
  
-    $("#doubleDamageList").append(`<li>${damageType2.damage_relations.double_damage_from[i].name}</li>`)
-    // If statement - check type for first and if it compares (use i -- )
+    // console.log(damageType2.damage_relations.double_damage_from[i].name)
+    $("#doubleDamageList").append(`<li>${damageType2.damage_relations.double_damage_from[i].name}</li>`);
+ 
+   
+
+// This creates an array containing all double damage weaknesses to the second type
+  
+doubleDamageArr2.push(damageType2.damage_relations.double_damage_from[i].name);
+
+// logs the new array for testing
+
+
   }
+
+  console.log("double damage array for second type is " + doubleDamageArr2);
 
   for (let i = 0; i < damageType2.damage_relations.half_damage_from.length; i++) {
     $("#halfDamageList").append(`<li>${damageType2.damage_relations.half_damage_from[i].name}</li>`)
+
+
+     // This creates an array containing all half damage weaknesses to the first type
+  
+   halfDamageArr2.push(damageType1.damage_relations.half_damage_from[i].name);
+
   }
+
+  console.log("half damage array for second type is " + halfDamageArr2);
 
   for (let i = 0; i < damageType2.damage_relations.no_damage_from.length; i++) {
     // console.log(damageType1.damage_relations.half_damage_from[i].name)
@@ -168,10 +229,16 @@ function assignDamage1(){
 
   }
 
-
-
   
-  // $doubleDamage.text(damageType1.damage_relations.double_damage_from['0'].name);
-  // console.log(damageType1.damage_relations.double_damage_from['0'].name)
+
+ 
+
+
+
+//pseudo code for what I want:
+// I want to check if any of the damage relation values are the same or different for both of the types
+// I want to take that and use it to add a value of 4x of 1/4 damage, which are currently not included in the API
+
+
 
 }
