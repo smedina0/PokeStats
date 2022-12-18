c617477df4859765089c28704a9609679ca1ef4c
Steven Medina <smedina2494@gmail.com>
2022-12-18 02:30:08 -0500

    WIP: Damage values are almost accurate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,374 @@
+// Object of all type weakness values:
+
+// START OF NEW CODE ADDED 12/17/22 AT 10:43 PM. IF CODE BREAKS - DELETE.
+
+const typeWeaknesses = {
+  "normal": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 2,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 1,
+    "ghost": 0,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 1
+  },
+  "fire": {
+    "normal": 1,
+    "fire": 0.5,
+    "water": 2,
+    "electric": 1,
+    "grass": 0.5,
+    "ice": 0.5,
+    "fighting": 1,
+    "poison": 1,
+    "ground": 2,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 0.5,
+    "rock": 2,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 0.5,
+    "fairy": 0.5
+  },
+  "water": {
+    "normal": 1,
+    "fire": 0.5,
+    "water": 0.5,
+    "electric": 2,
+    "grass": 2,
+    "ice": 0.5,
+    "fighting": 1,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 0.5,
+    "fairy": 1
+  },
+  "electric": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 0.5,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 1,
+    "poison": 1,
+    "ground": 2,
+    "flying": 0.5,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 0.5,
+    "fairy": 1
+  },
+  "grass": {
+    "normal": 1,
+    "fire": 2,
+    "water": 0.5,
+    "electric": 0.5,
+    "grass": 0.5,
+    "ice": 2,
+    "fighting": 1,
+    "poison": 2,
+    "ground": 0.5,
+    "flying": 2,
+    "psychic": 1,
+    "bug": 2,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 1
+  },
+  "ice": {
+    "normal": 1,
+    "fire": 2,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 0.5,
+    "fighting": 2,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 2,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 2,
+    "fairy": 1
+  },
+  "fighting": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 1,
+    "poison": 1,
+    "ground": 1,
+    "flying": 2,
+    "psychic": 2,
+    "bug": 0.5,
+    "rock": 0.5,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 0.5,
+    "steel": 1,
+    "fairy": 2
+  },
+  "poison": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 0.5,
+    "ice": 1,
+    "fighting": 0.5,
+    "poison": 0.5,
+    "ground": 2,
+    "flying": 1,
+    "psychic": 2,
+    "bug": 0.5,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 0.5
+  },
+  "ground": {
+    "normal": 1,
+    "fire": 1,
+    "water": 2,
+    "electric": 0,
+    "grass": 2,
+    "ice": 2,
+    "fighting": 1,
+    "poison": 0.5,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 0.5,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 1
+  },
+  "flying": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 2,
+    "grass": 0.5,
+    "ice": 2,
+    "fighting": 0.5,
+    "poison": 1,
+    "ground": 0,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 0.5,
+    "rock": 2,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 1
+  },
+  "psychic": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 0.5,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 0.5,
+    "bug": 2,
+    "rock": 1,
+    "ghost": 2,
+    "dragon": 1,
+    "dark": 2,
+    "steel": 1,
+    "fairy": 1
+  },
+  "bug": {
+    "normal": 1,
+    "fire": 2,
+    "water": 1,
+    "electric": 1,
+    "grass": 0.5,
+    "ice": 1,
+    "fighting": 0.5,
+    "poison": 1,
+    "ground": 0.5,
+    "flying": 2,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 2,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 1
+  },
+  "rock": {
+    "normal": 0.5,
+    "fire": 0.5,
+    "water": 2,
+    "electric": 1,
+    "grass": 2,
+    "ice": 1,
+    "fighting": 2,
+    "poison": 0.5,
+    "ground": 2,
+    "flying": 0.5,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 1,
+    "dark": 1,
+    "steel": 2,
+    "fairy": 1
+  },
+  "ghost": {
+    "normal": 0,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 0,
+    "poison": 0.5,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 0.5,
+    "rock": 1,
+    "ghost": 2,
+    "dragon": 1,
+    "dark": 2,
+    "steel": 1,
+    "fairy": 1
+  },
+  "dragon": {
+    "normal": 1,
+    "fire": 0.5,
+    "water": 0.5,
+    "electric": 0.5,
+    "grass": 0.5,
+    "ice": 2,
+    "fighting": 1,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 1,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 2,
+    "dark": 1,
+    "steel": 1,
+    "fairy": 2
+  },
+  "dark": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 2,
+    "poison": 1,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 0,
+    "bug": 2,
+    "rock": 1,
+    "ghost": 0.5,
+    "dragon": 1,
+    "dark": 0.5,
+    "steel": 1,
+    "fairy": 2
+  },
+  "steel": {
+    "normal": 0.5,
+    "fire": 2,
+    "water": 1,
+    "electric": 1,
+    "grass": 0.5,
+    "ice": 0.5,
+    "fighting": 2,
+    "poison": 0,
+    "ground": 2,
+    "flying": 0.5,
+    "psychic": 0.5,
+    "bug": 0.5,
+    "rock": 0.5,
+    "ghost": 0.5,
+    "dragon": 0.5,
+    "dark": 0.5,
+    "steel": 0.5,
+    "fairy": 0.5
+  },
+  "fairy": {
+    "normal": 1,
+    "fire": 1,
+    "water": 1,
+    "electric": 1,
+    "grass": 1,
+    "ice": 1,
+    "fighting": 0.5,
+    "poison": 2,
+    "ground": 1,
+    "flying": 1,
+    "psychic": 1,
+    "bug": 0.5,
+    "rock": 1,
+    "ghost": 1,
+    "dragon": 0,
+    "dark": 0.5,
+    "steel": 2,
+    "fairy": 1
+  }
+}
+
+
+// END OF NEW CODE - IF SOMETHING BREAKS DELETE EVERYTHING ABOVE
+
+
 // Pokemon Scripts
 let pokemonData;
 
@@ -157,7 +528,7 @@ function assignDamage1(){
 
     // This creates an array containing all double damage weaknesses to the first type
   
-   doubleDamageArr1.push(damageType1.damage_relations.double_damage_from[i].name);
+  //  doubleDamageArr1.push(damageType1.damage_relations.double_damage_from[i].name);
   }
 
    // logs the new array for testing
@@ -171,6 +542,8 @@ function assignDamage1(){
    halfDamageArr1.push(damageType1.damage_relations.half_damage_from[i].name);
   }
 
+
+  
   console.log("half damage array for first type is " + halfDamageArr1);
 
   for (let i = 0; i < damageType1.damage_relations.no_damage_from.length; i++) {
@@ -178,9 +551,67 @@ function assignDamage1(){
     $("#noDamageList").append(`<li>${damageType1.damage_relations.no_damage_from[i].name}</li>`)
 
   }
+
+  let $typeOneMultiplier;
+
+if(pokemonData.types.length == 1){
+  for(const property in typeWeaknesses){
+  console.log(`${property}: ${typeWeaknesses[$pokemonType1][property]}`);
+  }
+  $("#outputWeakness1").empty();
+
+  $.each(typeWeaknesses[$pokemonType1], function(key, value) {
+  
+  
+
+    $("#outputWeakness1").append("<li>" + key + ": " + value + "x </li>")
+   
+
+});
+} else if(pokemonData.types.length == 2){
+  for(const property in typeWeaknesses){
+    console.log(`${property}: ${typeWeaknesses[$pokemonType2][property]}`);
+    }
+  
+    $("#outputWeakness1").empty();
+  
+    $.each(typeWeaknesses[$pokemonType1], function(key, value1) {
+    
+    
+  
+      $("#outputWeakness1").append("<li>" + key + ": " + value1 + "x </li>")
+     
+      $typeOneMultiplier = value1;
+    });
+
+      $("#outputWeakness2").empty();
+
+      // console.log($typeOneMultiplier)
+      $.each(typeWeaknesses[$pokemonType2], function(key, value) {
+      
+      
+    
+        $("#outputWeakness2").append(`<li>${key}: ${value * $typeOneMultiplier}x </li>`);
+  
+  });
+
+   
+   
+     
+  
+ 
+
 }
 
 
+
+
+
+
+
+
+
+}
 // SECOND TYPE
 
 
@@ -228,17 +659,10 @@ doubleDamageArr2.push(damageType2.damage_relations.double_damage_from[i].name);
     $("#noDamageList").append(`<li>${damageType2.damage_relations.no_damage_from[i].name}</li>`)
 
   }
-
   
-
  
 
+  }
 
 
-//pseudo code for what I want:
-// I want to check if any of the damage relation values are the same or different for both of the types
-// I want to take that and use it to add a value of 4x of 1/4 damage, which are currently not included in the API
-
-
-
-}
+  
